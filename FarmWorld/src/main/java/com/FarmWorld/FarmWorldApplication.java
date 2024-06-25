c513a12b6f9402c3f1331ca6ddec9a78baa7fb02
dinesh kumar konda <kondadinesh4827@gmail.com>
2024-06-25 17:52:09 +0530

    added maven project

---
+++ FarmWorld/src/main/java/com/FarmWorld/FarmWorldApplication.java
@@ -0,0 +1,14 @@
+package com.FarmWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmWorldApplication.class, args);
+		System.out.println("Hello, i am d=from spring");
+	}
+
+}
---
+++ FarmWorld/src/test/java/com/FarmWorld/FarmWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FarmWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

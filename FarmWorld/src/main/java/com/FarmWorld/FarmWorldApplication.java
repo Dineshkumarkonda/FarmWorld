41d53f0ea44b78f6c5d71da9f8d421d8ee759bc4
Mithun_Posam <mithunposam@gmail.com>
2024-06-25 20:31:33 +0530

    Mithun joined

--- FarmWorld/src/main/java/com/FarmWorld/FarmWorldApplication.java
+++ FarmWorld/src/main/java/com/FarmWorld/FarmWorldApplication.java
@@ -8,6 +8,7 @@ public class FarmWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FarmWorldApplication.class, args);
+		System.out.println("Hello from Mithun");
 
 	}
 
